8f1d20535aa16c79042fc85206eb1c5ff765ec5d
ajjnix <ajjnix@gmail.com>
2015-08-30 17:57:31 +0600

    Initial Commit

---
+++ RouterAndXib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RouterAndXib
+//
+//  Created by ajjnix on 30/08/15.
+//  Copyright (c) 2015 ajjnix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RouterAndXib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RouterAndXib
+//
+//  Created by ajjnix on 30/08/15.
+//  Copyright (c) 2015 ajjnix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
